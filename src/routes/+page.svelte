af83cb5f26ffcc8f0f77d171146ae49fa8d39ef3
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-11 16:01:23 +0800

    feat: add numpad for the simulator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,27 @@
 <script>
-  let title = 'Nook'
+  import "../global.css";
 </script>
 
-<h1 class="title">Hello {title}!</h1>
+<div class="content">
+  <h1>Nook Attendance Tracker Simulator</h1>
+  <div class="tracker">
+    <div class="idInput">
+      <input type="text" class="idInput" />
+    </div>
+    <div class="numpad">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>0</button>
+    </div>
+    <div class="enter">
+      <button>Enter</button>
+    </div>
+  </div>
+</div>
