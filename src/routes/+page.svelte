f25a8a4da3174795997533889f8082f95e83ab71
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-11 16:08:43 +0800

    feat: adding input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,78 @@
 <script>
   import "../global.css";
+
+  let inputValue = "";
+
+  const clearValue = () => {
+    inputValue = "";
+  };
 </script>
 
 <div class="content">
   <h1>Nook Attendance Tracker Simulator</h1>
   <div class="tracker">
     <div class="idInput">
-      <input type="text" class="idInput" />
+      <input type="text" class="idInput" bind:value={inputValue} />
     </div>
     <div class="numpad">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>0</button>
+      <button
+        on:click={() => {
+          inputValue += "1";
+        }}>1</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "2";
+        }}>2</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "3";
+        }}>3</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "4";
+        }}>4</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "5";
+        }}>5</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "6";
+        }}>6</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "7";
+        }}>7</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "8";
+        }}>8</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "9";
+        }}>9</button
+      >
+      <button
+        on:click={() => {
+          inputValue += "0";
+        }}>0</button
+      >
     </div>
     <div class="enter">
-      <button>Enter</button>
+      <button
+        on:click={() => {
+          alert("ID Number: " + inputValue);
+        }}>Enter</button
+      >
+      <button on:click={clearValue}>Clear</button>
     </div>
   </div>
 </div>
